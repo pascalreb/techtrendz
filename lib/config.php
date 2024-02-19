f42720bff7afa9bb9eaed9a291610f957913d635
Pascal Reb <pascal.reb10@gmail.com>
2024-02-19 14:58:02 +0100

    Ignore config.php

--- lib/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("_DOMAIN_", "localhost");
-define("_ARTICLES_IMAGES_FOLDER_", "uploads/articles/");
-define("_ASSETS_IMAGES_FOLDER_", "assets/images/");
-define("_ADMIN_ITEM_PER_PAGE_", 10);
-define("_DB_NAME_", "techtrendz");
-define("_DB_USER_", "pascalr");
-define("_DB_PASSWORD_", "PUxitet2*");
-define("_APP_EMAIL_", "kalour10@hotmail.fr");
